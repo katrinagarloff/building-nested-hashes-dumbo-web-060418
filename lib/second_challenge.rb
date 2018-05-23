6fab0cd89af8d1cecddb9ca4ee26361b881be2ce
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-23 20:47:33 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,8 @@ def second_challenge
       patriarch: {}, 
       matriarch: {},
       hero: {},
-      hero_friends: []}
+      hero_friends: []
+    },
     :capulet => {
       patriarch: {}, 
       matriarch: {},
